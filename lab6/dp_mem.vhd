106ced43979451161d3db08894d886177a591d34
whowe444 <willhowe235@gmail.com>
2025-05-11 20:02:51 -0400

    lab6: don't need to duplicate the dp_mem entity since it is pulled in from coregen

--- lab6/dp_mem.vhd
+++ lab6/dp_mem.vhd
@@ -2,20 +2,6 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use IEEE.std_logic_unsigned.conv_integer;
 
-ENTITY dp_mem IS
-	port (
-	addra: IN std_logic_VECTOR(3 downto 0);
-	addrb: IN std_logic_VECTOR(3 downto 0);
-	clka: IN std_logic;
-	clkb: IN std_logic;
-	dina: IN std_logic_VECTOR(15 downto 0);
-	dinb: IN std_logic_VECTOR(15 downto 0);
-	douta: OUT std_logic_VECTOR(15 downto 0);
-	doutb: OUT std_logic_VECTOR(15 downto 0);
-	wea: IN std_logic;
-	web: IN std_logic);
-END dp_mem;
-
 architecture RTL of dp_mem IS
 
 type MEMTYPE is array(15 downto 0) of std_logic_vector(15 downto 0);
